5cfa2ff7862acfc4c5824f07011e155862e3af2a
buschco <colin@busch.dev>
2023-01-01 15:53:48 +0100

    add format and fix colors

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,14 +12,17 @@ require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- Theme
-  use 'buschco/vim-horizon'
+  -- use 'buschco/vim-horizon'
+  use '~/Documents/code.nosync/vim-horizon'
 
   use 'tpope/vim-surround'
   use 'tpope/vim-unimpaired'
   use 'tpope/vim-repeat'
   use 'unblevable/quick-scope'
+  use 'norcalli/nvim-colorizer.lua'
 
   use { 'neovim/nvim-lspconfig', requires = { 'j-hui/fidget.nvim', 'folke/neodev.nvim', } }
+  use "lukas-reineke/lsp-format.nvim"
   use 'jose-elias-alvarez/null-ls.nvim'
 
   use { -- Autocompletion
@@ -210,6 +213,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
 ft_to_parser.javascriptreact = "tsx"
 
+require'colorizer'.setup()
+
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
   ensure_installed = {
@@ -335,8 +340,16 @@ require('gitsigns').setup {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
-    layout_strategy = 'vertical',
+    preview = {
+      treesitter = true,
+      title = false,
+    },
+    results_title = false,
+    prompt_title = false,
+    layout_strategy = 'center',
     layout_config = {
+      anchor = 'N',
+      prompt_position = 'bottom',
       vertical = { width = 0.7 },
     },
     mappings = {
@@ -394,13 +407,18 @@ vim.keymap.set('n', '<space>a', function()
     severity_limit = vim.diagnostic.severity["WARN"]
   })
 end, { desc = 'list buffers' })
+
 require('Comment').setup()
 
-local null_ls = require("null-ls")
+require("lsp-format").setup {}
 
+local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.formatting.eslint,
     null_ls.builtins.diagnostics.cspell.with({
       extra_args = { "--config", vim.fn.expand("~/.cspell.json") },
       diagnostics_postprocess = function(diagnostic) diagnostic.severity = vim.diagnostic.severity["HINT"] end,
@@ -408,16 +426,7 @@ null_ls.setup({
     -- null_ls.builtins.code_actions.cspell,
     null_ls.builtins.formatting.prettier
   },
-  on_attach = function(client, bufnr)
-    if client.supports_method 'textDocument/formatting' then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = augroup,
-        buffer = bufnr,
-        callback = function() vim.lsp.buf.format({ bufnr = bufnr }) end,
-      })
-    end
-  end,
+  on_attach = require("lsp-format").on_attach
 })
 
 -- lsp setup
@@ -441,23 +450,22 @@ local on_attach = function(_, bufnr)
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+  -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  -- nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  -- nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+  -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  -- nmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, '[W]orkspace [L]ist Folders')
 end
 
 local lspconfig = require('lspconfig')
